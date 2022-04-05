cfeba89ea12d6496a6f55f3e1e6d1b220a1c585e
ajswebdesigns <andrew@ajswebdesigns.com>
2022-04-05 19:08:34 -0400

    Second Commit

--- index.js
+++ index.js
@@ -2,12 +2,24 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const port = 3000
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/farmStandDB')
+.then(()=>{
+  console.log('Mongo Connection Open')
+})
+.catch((err)=>{
+  console.log('OH No Mongo ERROR')
+  console.log(err)
+})
 
-app.set('views', paht.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-
+app.get('/',(req, res)=>{
+  res.send('Thank you for visiting our website')
+})
 
 app.listen(port, ()=>{
   console.log(`App is listening on ${port}`);
 })
+
