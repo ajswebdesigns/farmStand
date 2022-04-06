ba747868b64f5d5256cc82d6bb1b1768e11215a7
ajswebdesigns <andrew@ajswebdesigns.com>
2022-04-06 17:20:43 -0400

    post route complete

--- index.js
+++ index.js
@@ -15,19 +15,33 @@ mongoose.connect('mongodb://localhost:27017/farmStandDB')
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+// Allows us to parse the body
+app.use(express.urlencoded({extended:true}))
 
-// All Products Route
+// All Products Route:
 app.get('/', async (req, res)=>{
-  const products =  await Product.find({})
-  res.render('products/index',{ products })
+  const products =  await Product.find({});
+  res.render('products/index',{ products });
 })
 
-// Product Details Route
+// Create Product Route:
+app.get('/products/new',(req,res)=>{
+  res.render('products/new');
+})
+
+// Post Route For New Product:
+app.post('/products',async (req, res)=>{
+  const newProduct = new Product({name: req.body.name, price: req.body.price, category: req.body.category})
+  await newProduct.save()
+  res.redirect('/')
+})
+
+// Product Details Route:
 app.get('/products/:id', async (req, res)=>{
   const {id} = req.params;
-  const product =  await Product.findById(id)
-  console.log(product)
-  res.render('products/show',{product})
+  const product =  await Product.findById(id);
+  console.log(product);
+  res.render('products/show',{product});
 })
 
 app.listen(port, ()=>{
