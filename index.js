76e3c905bad6d9f01aed4ea1e19644bdd16fee15
ajswebdesigns <andrew@ajswebdesigns.com>
2022-04-05 20:13:29 -0400

    third

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require('path');
 const port = 3000
 const mongoose = require('mongoose');
+const Product = require('./models/product');
 mongoose.connect('mongodb://localhost:27017/farmStandDB')
 .then(()=>{
   console.log('Mongo Connection Open')
@@ -15,11 +16,23 @@ mongoose.connect('mongodb://localhost:27017/farmStandDB')
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.get('/',(req, res)=>{
-  res.send('Thank you for visiting our website')
+app.get('/', async (req, res)=>{
+  const products =  await Product.find({})
+  console.log(products)
+  res.send('All Products Will be here!')
 })
 
 app.listen(port, ()=>{
   console.log(`App is listening on ${port}`);
 })
 
+
+
+// old way of doing things async is better
+// app.get('/', (req, res)=>{
+//   const products = Product.find({})
+//   .then((res)=>{
+//     console.log(res)
+//   })
+//   res.send('All Products Will be here!')
+// })
