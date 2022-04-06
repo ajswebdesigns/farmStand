b2b95011f543e8b0d6d904bd22c039a5b118b192
ajswebdesigns <andrew@ajswebdesigns.com>
2022-04-06 16:04:58 -0400

    great

--- index.js
+++ index.js
@@ -16,11 +16,13 @@ mongoose.connect('mongodb://localhost:27017/farmStandDB')
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// All Products Route
 app.get('/', async (req, res)=>{
   const products =  await Product.find({})
   res.render('products/index',{ products })
 })
 
+// Product Details Route
 app.get('/products/:id', async (req, res)=>{
   const {id} = req.params;
   const product =  await Product.findById(id)
