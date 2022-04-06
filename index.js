6db5972fdf31a3e259e1eb43e6d8c80d3b0d5d92
ajswebdesigns <andrew@ajswebdesigns.com>
2022-04-06 18:28:44 -0400

    update route complete

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require('path');
 const port = 3000
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 const Product = require('./models/product');
 mongoose.connect('mongodb://localhost:27017/farmStandDB')
 .then(()=>{
@@ -16,7 +17,8 @@ mongoose.connect('mongodb://localhost:27017/farmStandDB')
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 // Allows us to parse the body
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({extended:true}));
+app.use(methodOverride('_method'));
 
 // All Products Route:
 app.get('/', async (req, res)=>{
@@ -31,9 +33,9 @@ app.get('/products/new',(req,res)=>{
 
 // Post Route For New Product:
 app.post('/products',async (req, res)=>{
-  const newProduct = new Product({name: req.body.name, price: req.body.price, category: req.body.category})
-  await newProduct.save()
-  res.redirect('/')
+  const newProduct = new Product({name: req.body.name, price: req.body.price, category: req.body.category});
+  await newProduct.save();
+  res.redirect('/');
 })
 
 // Product Details Route:
@@ -42,6 +44,19 @@ app.get('/products/:id', async (req, res)=>{
   const product =  await Product.findById(id);
   console.log(product);
   res.render('products/show',{product});
+})
+
+// Update Product Route: 
+app.get('/products/:id/edit', async (req, res)=>{
+  const {id} = req.params;
+  const product =  await Product.findById(id);
+  res.render('products/edit',{product});
+})
+
+app.put('/products/:id', async(req, res)=>{
+   const {id} = req.params;
+   const product = await Product.findByIdAndUpdate(id, req.body, {runValidators:true, new: true})
+   res.redirect(`/products/${product.id}`);
 })
 
 app.listen(port, ()=>{
